ac427bf580ec6a99e9d3e2ab2b07d07a4eb35288
LI <jl362@uw.edu>
2024-03-08 16:44:50 -0800

    Connect to MongoDB and hide the url by .env

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// used for enable the ".env" file
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("mongodb is connected");
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(3000, () => {
 
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 3000!");
 });
