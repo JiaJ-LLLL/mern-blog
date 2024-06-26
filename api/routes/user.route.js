5320b87369ef8206a681725f66f0b268d0b0f7f8
LI <jl362@uw.edu>
2024-06-25 18:54:14 -0700

    Add delete user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,7 @@
 // Simply list of api and calls to the controller
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
-import { updateUser } from '../controllers/user.controller.js';
+import { updateUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ const router = express.Router();
 router.get('/test', test);
 
 router.put('/update/:userId', verifyToken, updateUser);
+
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 export default router;
