dabb4798744abb2e03c81812d924c80e469bab6f
LI <jl362@uw.edu>
2024-08-25 23:23:18 -0700

    Add recent articles to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useParams, Link } from 'react-router-dom'
 import { Spinner, Button } from 'flowbite-react'
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 
 
@@ -12,6 +13,8 @@ export default function PostPage() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+
+  const [recentPosts, setRecentPosts] = useState(null);
 
 
   useEffect(()=> {
@@ -39,6 +42,23 @@ export default function PostPage() {
     fetchPost();
   }, [postSlug]); 
 
+  
+  useEffect(() => {
+    const fetchRecentPosts = async () => {
+      try {
+        const res = await fetch('/api/post/getposts?limit=3');
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }    
+    }
+    fetchRecentPosts();
+  }, [])
+
+
   if (loading) return <div className='flex justify-center items-center min-h-screen'>
     <Spinner size='xl'/>
   </div>
@@ -62,6 +82,15 @@ export default function PostPage() {
         <CallToAction />
       </div>
       <CommentSection postId={post._id}/>
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent Articles</h1>
+        <div>
+          {
+            // can't put into a functio, bc recentPosts is null at init
+            recentPosts && recentPosts.map((post) => <PostCard key={post._id} post={post}></PostCard>)
+          }
+        </div>
+      </div>
     </main>
   )
 }
